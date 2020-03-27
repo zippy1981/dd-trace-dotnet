66564b80a22dd01bfbd5cc38c3e2666ff7185d34
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-03-27 18:33:59 -0400

    wip

--- performance/Datadog.RuntimeMetrics/GcMetrics.cs
+++ performance/Datadog.RuntimeMetrics/GcMetrics.cs
@@ -2,12 +2,13 @@ namespace Datadog.RuntimeMetrics
 {
     public struct GcMetrics
     {
-        public long Allocated;
-        public long WorkingSet;
-        public long PrivateBytes;
+        public long TotalAllocatedBytes;
+        public long WorkingSetBytes;
+        public long PrivateMemoryBytes;
         public int GcCountGen0;
         public int GcCountGen1;
         public int GcCountGen2;
-        public double CpuUsage;
+        public double CpuTimeMs;
+        public double CpuPercent;
     }
 }
--- performance/Datadog.RuntimeMetrics/GcMetricsBackgroundService.cs
+++ performance/Datadog.RuntimeMetrics/GcMetricsBackgroundService.cs
@@ -12,9 +12,11 @@ namespace Datadog.RuntimeMetrics
         private readonly Process _process = Process.GetCurrentProcess();
         private readonly GcEventListener _gcEventListener = new GcEventListener();
 
-        private TimeSpan _oldCpuTime = TimeSpan.Zero;
+        private TimeSpan _oldCpuTime;
         private DateTime _lastMonitorTime = DateTime.UtcNow;
-        private double _cpu;
+        private int _lastGcCountGen0;
+        private int _lastGcCountGen1;
+        private int _lastGcCountGen2;
 
         public override Task StartAsync(CancellationToken cancellationToken)
         {
@@ -32,20 +34,26 @@ namespace Datadog.RuntimeMetrics
         {
             return Task.Factory.StartNew(() =>
                                          {
-                                             var values = new MetricValue[7];
+                                             var values = new MetricValue[8];
                                              var period = TimeSpan.FromSeconds(1);
+
+                                             // set initial values
+                                             _process.Refresh();
+                                             _oldCpuTime = _process.TotalProcessorTime;
+                                             _lastMonitorTime = DateTime.UtcNow;
 
                                              while (!stoppingToken.IsCancellationRequested)
                                              {
                                                  GcMetrics metrics = GetMetrics();
 
-                                                 values[0] = new MetricValue(Metric.GcHeapSize, metrics.Allocated);
-                                                 values[1] = new MetricValue(Metric.WorkingSet, metrics.WorkingSet);
-                                                 values[2] = new MetricValue(Metric.PrivateBytes, metrics.PrivateBytes);
+                                                 values[0] = new MetricValue(Metric.GcHeapSize, metrics.TotalAllocatedBytes);
+                                                 values[1] = new MetricValue(Metric.WorkingSet, metrics.WorkingSetBytes);
+                                                 values[2] = new MetricValue(Metric.PrivateBytes, metrics.PrivateMemoryBytes);
                                                  values[3] = new MetricValue(Metric.GcCountGen0, metrics.GcCountGen0);
                                                  values[4] = new MetricValue(Metric.GcCountGen1, metrics.GcCountGen1);
                                                  values[5] = new MetricValue(Metric.GcCountGen2, metrics.GcCountGen2);
-                                                 values[6] = new MetricValue(Metric.CpuUsage, metrics.CpuUsage);
+                                                 values[6] = new MetricValue(Metric.CpuPercent, metrics.CpuPercent);
+                                                 values[7] = new MetricValue(Metric.CpuTimeMs, metrics.CpuTimeMs);
 
                                                  _observers.OnNext(values);
 
@@ -64,26 +72,34 @@ namespace Datadog.RuntimeMetrics
         {
             DateTime now = DateTime.UtcNow;
             _process.Refresh();
-
-            double cpuElapsedTime = now.Subtract(_lastMonitorTime).TotalMilliseconds;
-
             TimeSpan newCpuTime = _process.TotalProcessorTime;
-            double elapsedCpu = (newCpuTime - _oldCpuTime).TotalMilliseconds;
-            _cpu = elapsedCpu * 100 / Environment.ProcessorCount / cpuElapsedTime;
+
+            TimeSpan elapsedTime = now - _lastMonitorTime;
+            TimeSpan elapsedCpuTime = newCpuTime - _oldCpuTime;
+            double cpu = 100.0 * elapsedCpuTime.Ticks / elapsedTime.Ticks /* / Environment.ProcessorCount */;
 
             _lastMonitorTime = now;
             _oldCpuTime = newCpuTime;
 
+            int gcCountGen0 = GC.CollectionCount(0);
+            int gcCountGen1 = GC.CollectionCount(1);
+            int gcCountGen2 = GC.CollectionCount(2);
+
             var metrics = new GcMetrics
                           {
-                              Allocated = GC.GetTotalMemory(false),
-                              WorkingSet = _process.WorkingSet64,
-                              PrivateBytes = _process.PrivateMemorySize64,
-                              GcCountGen0 = GC.CollectionCount(0),
-                              GcCountGen1 = GC.CollectionCount(1),
-                              GcCountGen2 = GC.CollectionCount(2),
-                              CpuUsage = _cpu
+                              TotalAllocatedBytes = GC.GetTotalMemory(false),
+                              WorkingSetBytes = _process.WorkingSet64,
+                              PrivateMemoryBytes = _process.PrivateMemorySize64,
+                              GcCountGen0 = gcCountGen0 - _lastGcCountGen0,
+                              GcCountGen1 = gcCountGen1 - _lastGcCountGen1,
+                              GcCountGen2 = gcCountGen2 - _lastGcCountGen2,
+                              CpuTimeMs = elapsedCpuTime.TotalMilliseconds,
+                              CpuPercent = cpu
                           };
+
+            _lastGcCountGen0 = gcCountGen0;
+            _lastGcCountGen1 = gcCountGen1;
+            _lastGcCountGen2 = gcCountGen2;
 
             return metrics;
         }
