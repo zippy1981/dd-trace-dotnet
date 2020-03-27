e313c421b3a4060e9524b657df298036896ebd01
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-03-27 12:50:35 -0400

    add more Metrics, add MetricValue.Tags

--- performance/Datadog.RuntimeMetrics/Metric.cs
+++ performance/Datadog.RuntimeMetrics/Metric.cs
@@ -5,12 +5,21 @@ namespace Datadog.RuntimeMetrics
     public class Metric
     {
         public static readonly Metric GcHeapSize = new Metric("dotnet_counters.gc_heap_size", MetricType.Gauge);
-        public static readonly Metric WorkingSet = new Metric("dotnet_counters.working_set", MetricType.Gauge);
-        public static readonly Metric PrivateBytes = new Metric("dotnet_counters.private_bytes", MetricType.Gauge);
         public static readonly Metric GcCountGen0 = new Metric("dotnet_counters.gen_0_gc_count", MetricType.Counting);
         public static readonly Metric GcCountGen1 = new Metric("dotnet_counters.gen_1_gc_count", MetricType.Counting);
         public static readonly Metric GcCountGen2 = new Metric("dotnet_counters.gen_2_gc_count", MetricType.Counting);
+        public static readonly Metric GcSizeGen0 = new Metric("dotnet_counters.gc_size.gen_0", MetricType.Gauge);
+        public static readonly Metric GcSizeGen1 = new Metric("dotnet_counters.gc_size.gen_1", MetricType.Gauge);
+        public static readonly Metric GcSizeGen2 = new Metric("dotnet_counters.gc_size.gen_2", MetricType.Gauge);
+        public static readonly Metric GcSizeLoh = new Metric("dotnet_counters.gc_size.loh", MetricType.Gauge);
+        public static readonly Metric WorkingSet = new Metric("dotnet_counters.working_set", MetricType.Gauge);
+        public static readonly Metric PrivateBytes = new Metric("dotnet_counters.private_bytes", MetricType.Gauge);
         public static readonly Metric CpuUsage = new Metric("dotnet_counters.cpu_usage", MetricType.Gauge);
+        public static readonly Metric AllocatedBytes = new Metric("dotnet_counters.alloc", MetricType.Counting);
+
+        public static class HeapStats
+        {
+        }
 
         public string Name { get; }
         public MetricType Type { get; }
--- performance/Datadog.RuntimeMetrics/MetricValue.cs
+++ performance/Datadog.RuntimeMetrics/MetricValue.cs
@@ -4,11 +4,20 @@ namespace Datadog.RuntimeMetrics
     {
         public Metric Metric;
         public double Value;
+        public string[]? Tags;
 
         public MetricValue(Metric metric, double value)
+            : this(metric, value, tags: null)
         {
             Metric = metric;
             Value = value;
+        }
+
+        public MetricValue(Metric metric, double value, string[]? tags)
+        {
+            Metric = metric;
+            Value = value;
+            Tags = tags;
         }
     }
 }
