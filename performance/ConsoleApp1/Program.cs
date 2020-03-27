0e2fddf4a919acb59a063406c4e0c99aea6f33b8
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-03-27 10:38:11 -0400

    EventListener spike

--- performance/ConsoleApp1/Program.cs
+++ performance/ConsoleApp1/Program.cs
@@ -1,65 +1,17 @@
 using System;
-using System.Diagnostics;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Diagnostics.Tracing;
-using Microsoft.Diagnostics.Tracing.Parsers;
-using Microsoft.Diagnostics.Tracing.Parsers.Clr;
-using Microsoft.Diagnostics.Tracing.Session;
+using Datadog.RuntimeMetrics;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static async Task Main()
         {
-            int processId;
+            var listener = new SimpleEventListener();
 
-            using (var process = Process.GetCurrentProcess())
-            {
-                processId = process.Id;
-            }
+            await Task.Delay(TimeSpan.FromSeconds(5));
 
-            using (var session = new TraceEventSession("MyRealTimeSession")) // Create a session to listen for events
-            {
-                Console.CancelKeyPress += (sender, args) =>
-                                          {
-                                              Console.WriteLine("Exiting...");
-                                              session.Dispose();
-                                          };
-
-                byte[] bytes;
-
-                Task.Run(() =>
-                         {
-                             while (true)
-                             {
-                                 bytes = new byte[1024];
-                                 Thread.Sleep(10);
-                             }
-                         });
-
-                session.Source.Clr.GCTriggered += data =>
-                                                  {
-                                                      if (data.ProcessID == processId)
-                                                      {
-                                                          Console.WriteLine(data);
-                                                      }
-                                                  };
-
-                /*
-                session.Source.Clr.GCHeapStats += data =>
-                                                  {
-                                                      if (data.ProcessID == processId)
-                                                      {
-                                                          Console.WriteLine(data);
-                                                      }
-                                                  };
-                */
-
-                session.EnableProvider(ClrTraceEventParser.ProviderGuid);
-                session.Source.Process();
-            }
         }
     }
 }
