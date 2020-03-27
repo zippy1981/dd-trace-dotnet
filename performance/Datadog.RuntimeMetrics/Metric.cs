0bbd0f5459e35e220bb21fc1ffd64585b24d8c2b
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-03-27 18:58:04 -0400

    subscribe to the EventListener

--- performance/Datadog.RuntimeMetrics/Metric.cs
+++ performance/Datadog.RuntimeMetrics/Metric.cs
@@ -7,12 +7,12 @@ namespace Datadog.RuntimeMetrics
     public class Metric
     {
         public static readonly Metric GcHeapSize = new Metric("dotnet_counters.gc_heap_size", MetricType.Gauge);
-        public static readonly Metric GcCountGen0 = new Metric("dotnet_counters.gen_0_gc_count", MetricType.Counting);
-        public static readonly Metric GcCountGen1 = new Metric("dotnet_counters.gen_1_gc_count", MetricType.Counting);
-        public static readonly Metric GcCountGen2 = new Metric("dotnet_counters.gen_2_gc_count", MetricType.Counting);
-        public static readonly Metric GcSizeGen0 = new Metric("dotnet_counters.gc_size.gen_0", MetricType.Gauge);
-        public static readonly Metric GcSizeGen1 = new Metric("dotnet_counters.gc_size.gen_1", MetricType.Gauge);
-        public static readonly Metric GcSizeGen2 = new Metric("dotnet_counters.gc_size.gen_2", MetricType.Gauge);
+        public static readonly Metric GcCountGen0 = new Metric("dotnet_counters.gc_count_gen_0", MetricType.Counting);
+        public static readonly Metric GcCountGen1 = new Metric("dotnet_counters.gc_count_gen_1", MetricType.Counting);
+        public static readonly Metric GcCountGen2 = new Metric("dotnet_counters.gc_count_gen_2", MetricType.Counting);
+        public static readonly Metric GcSizeGen0 = new Metric("dotnet_counters.gc_size_gen_0", MetricType.Gauge);
+        public static readonly Metric GcSizeGen1 = new Metric("dotnet_counters.gc_size_gen_1", MetricType.Gauge);
+        public static readonly Metric GcSizeGen2 = new Metric("dotnet_counters.gc_size_gen_2", MetricType.Gauge);
         public static readonly Metric GcSizeLoh = new Metric("dotnet_counters.gc_size.loh", MetricType.Gauge);
         public static readonly Metric WorkingSet = new Metric("dotnet_counters.working_set", MetricType.Gauge);
         public static readonly Metric PrivateBytes = new Metric("dotnet_counters.private_bytes", MetricType.Gauge);
