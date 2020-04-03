05a3280ed396d569b1dbd030ca4969825a975fab
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-04-02 23:04:03 -0400

    wip benchmarks

--- benchmarks/Datadog.Trace.Benchmarks/Benchmarks.cs
+++ benchmarks/Datadog.Trace.Benchmarks/Benchmarks.cs
@@ -1,55 +1,63 @@
 using System.Reflection;
-using System.Threading;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using Datadog.Trace.ExtensionMethods;
 
 namespace Datadog.Trace.Benchmarks
 {
     [MemoryDiagnoser]
     [GcServer(true)]
-    [GcForce(false)]
-    [Config(typeof(Config))]
+    [GcForce(true)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp21)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    //[SimpleJob(RuntimeMoniker.Net48)]
+    //[Config(typeof(Config))]
     public class Benchmarks
     {
-        [Params(1, 5, 10, 20, 40)]
-        public int SpanCount { get; set; }
+        [Params(100, 200)]
+        public int TraceCount { get; set; }
+
+        [Params(1, 10)]
+        public int SpansPerTrace { get; set; }
 
         private static readonly MethodInfo Flush = typeof(Tracer).GetMethod("FlushAsync", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 
         [Benchmark]
         public void WithTraces()
         {
-            using (Scope rootScope = Tracer.Instance.StartActive("root"))
+            for (int traceIndex = 0; traceIndex < TraceCount; traceIndex++)
             {
-                Span rootSpan = rootScope.Span;
-                rootSpan.Type = SpanTypes.Custom;
-                rootSpan.SetTag("traceIndex", "0");
-                rootSpan.SetTag("key1", "value1");
-                rootSpan.SetTag("key2", "value2");
-                rootSpan.SetTraceSamplingPriority(SamplingPriority.UserReject);
-
-                for (int spanIndex = 0; spanIndex < SpanCount - 1; spanIndex++)
+                using (Scope rootScope = Tracer.Instance.StartActive("root"))
                 {
-                    using (Scope childScope = Tracer.Instance.StartActive("child"))
+                    Span rootSpan = rootScope.Span;
+                    rootSpan.Type = SpanTypes.Custom;
+                    rootSpan.SetTag("traceIndex", "0");
+                    AddTags(rootSpan, 10);
+                    rootSpan.SetTraceSamplingPriority(SamplingPriority.UserReject);
+
+                    for (int spanIndex = 0; spanIndex < SpansPerTrace - 1; spanIndex++)
                     {
-                        Span childSpan = childScope.Span;
-                        childSpan.Type = SpanTypes.Custom;
-                        childSpan.SetTag("spanIndex", spanIndex.ToString());
-                        childSpan.SetTag("key1", "value1");
-                        childSpan.SetTag("key2", "value2");
-
-                        Thread.Sleep(5);
+                        using (Scope childScope = Tracer.Instance.StartActive("child"))
+                        {
+                            Span childSpan = childScope.Span;
+                            childSpan.Type = SpanTypes.Custom;
+                            childSpan.SetTag("spanIndex", spanIndex.ToString());
+                            AddTags(rootSpan, 10);
+                        }
                     }
-
-                    Thread.Sleep(5);
                 }
-
-                Thread.Sleep(5);
             }
 
-            var task = Flush.Invoke(Tracer.Instance, null) as Task;
-            task.GetAwaiter().GetResult();
+            ((Task)Flush.Invoke(Tracer.Instance, null)).GetAwaiter().GetResult();
+        }
+
+        private void AddTags(Span span, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                span.SetTag($"key{i}", $"value{i}");
+            }
         }
     }
 }
