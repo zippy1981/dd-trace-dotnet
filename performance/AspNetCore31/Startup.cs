86a34624796b266efb48e270e887db06ab859609
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-04-14 12:38:28 -0400

    random span and tag counts (with configurable max), misc docker-compose improvements

--- performance/AspNetCore31/Startup.cs
+++ performance/AspNetCore31/Startup.cs
@@ -44,21 +44,20 @@ namespace AspNetCore31
                 app.UseDeveloperExceptionPage();
             }
 
-            int manualSpanCount = Configuration.GetValue("DD_MANUAL_SPAN_COUNT", 0);
+            int maxSpans = Configuration.GetValue("DD_MAX_MANUAL_SPANS", 0);
 
-            if (manualSpanCount > 0)
+            if (maxSpans > 0)
             {
                 // hack: internal method
                 tracer.GetType()
                       .GetMethod("StartDiagnosticObservers", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
                      ?.Invoke(tracer, null);
 
+                int maxTags = Configuration.GetValue("DD_MAX_TAGS", 0);
+
                 // first span is created with DiagnosticSource,
                 // additional spans are created manually in middleware
-                if (manualSpanCount > 1)
-                {
-                    app.UseDatadogTracing(tracer, manualSpanCount);
-                }
+                app.UseDatadogTracing(tracer, maxSpans - 1, maxTags);
             }
 
             app.Run(async context =>
@@ -70,22 +69,24 @@ namespace AspNetCore31
 
         private IEnumerable<string> GetMetricsTags()
         {
-            int manualSpanCount = Configuration.GetValue("DD_MANUAL_SPAN_COUNT", 0);
+            int maxSpans = Configuration.GetValue("DD_MAX_MANUAL_SPANS", 0);
+            int maxTags = Configuration.GetValue("DD_MAX_TAGS", 0);
             string tracerVersion = Configuration["DD_TRACER_VERSION"];
-            string[] tags = new string[4];
+            string[] tags = new string[5];
 
             tags[0] = "service_name:AspNetCore31";
-            tags[1] = $"span_count:{manualSpanCount}";
+            tags[1] = $"max_spans:{maxSpans}";
+            tags[2] = $"max_tags:{maxTags}";
 
-            if (manualSpanCount == 0)
+            if (maxSpans == 0)
             {
-                tags[2] = "tracer_mode:none";
-                tags[3] = "tracer_version:none";
+                tags[3] = "tracer_mode:none";
+                tags[4] = "tracer_version:none";
             }
             else
             {
-                tags[2] = "tracer_mode:manual";
-                tags[3] = $"tracer_version:{tracerVersion}";
+                tags[3] = "tracer_mode:manual";
+                tags[4] = $"tracer_version:{tracerVersion}";
             }
 
             return tags;
