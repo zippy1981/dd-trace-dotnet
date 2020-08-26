71a48e8ea9a3b215c46c13e5d4d2448cca25dd49
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-08-26 13:52:55 -0400

    add quickie NpgsqlTest solution with two projects (console app and data access library)

---
+++ reproductions/NpgsqlTest/NpgsqlTest.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+using NpgsqlTest.DataAccess;
+
+namespace NpgsqlTest.Cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string postgresHost = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+            Console.WriteLine($"Opening connection to {postgresHost}...");
+
+            using (var client = new PostgresClient($"Host={postgresHost};Username=postgres;Password=postgres;Database=postgres"))
+            {
+                Console.WriteLine("Excuting query...");
+
+                using (DbDataReader reader = await client.Query("SELECT 1;"))
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        object value = reader[0];
+                        Console.WriteLine($"Value = {value}");
+                    }
+                }
+            }
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ reproductions/NpgsqlTest/NpgsqlTest.DataAccess/PostgresClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace NpgsqlTest.DataAccess
+{
+    public class PostgresClient : IDisposable
+    {
+        private readonly DbCommand _command;
+
+        public PostgresClient(string connectionString)
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            _command = new NpgsqlCommand("", connection);
+        }
+
+        public async Task<DbDataReader> Query(string commandText)
+        {
+            _command.CommandText = commandText;
+            return await _command.ExecuteReaderAsync();
+        }
+
+        public void Dispose()
+        {
+            _command?.Dispose();
+        }
+    }
+}
