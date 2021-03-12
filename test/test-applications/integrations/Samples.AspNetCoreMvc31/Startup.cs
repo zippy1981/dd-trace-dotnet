398506740146e03b9f5bddd0a104058b9218097a
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2021-03-12 00:09:34 -0500

    TEMP: Samples.AspNetCoreMvc31

--- test/test-applications/integrations/Samples.AspNetCoreMvc31/Startup.cs
+++ test/test-applications/integrations/Samples.AspNetCoreMvc31/Startup.cs
@@ -1,3 +1,4 @@
+using Datadog.Trace.DiagnosticSource;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,7 @@ namespace Samples.AspNetCoreMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDatadogTracing();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
