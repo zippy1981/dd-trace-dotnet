5a20b21067891a56d58c04ba895ddc062cbbde61
Tony Redondo <tony.redondo@datadoghq.com>
2021-06-22 19:09:57 +0200

    Changes.

--- src/Datadog.Shared.Native/macros.h
+++ src/Datadog.Shared.Native/macros.h
@@ -30,4 +30,45 @@
         if ((EXPR) == false) return E_FAIL;                                                                            \
     } while (0)
 
+
+#define CheckIfTrue(EXPR)                                                                                              \
+    static int sValue = -1;                                                                                            \
+    if (sValue == -1)                                                                                                  \
+    {                                                                                                                  \
+        const auto envValue = EXPR;                                                                                    \
+        sValue = envValue == WStr("1") || envValue == WStr("true") ? 1 : 0;                                            \
+    }                                                                                                                  \
+    return sValue == 1;
+
+#define CheckIfFalse(EXPR)                                                                                             \
+    static int sValue = -1;                                                                                            \
+    if (sValue == -1)                                                                                                  \
+    {                                                                                                                  \
+        const auto envValue = EXPR;                                                                                    \
+        sValue = envValue == WStr("0") || envValue == WStr("false") ? 1 : 0;                                           \
+    }                                                                                                                  \
+    return sValue == 1;
+
+#define ToBooleanWithDefault(EXPR, DEFAULT)                                                                            \
+    static int sValue = -1;                                                                                            \
+    if (sValue == -1)                                                                                                  \
+    {                                                                                                                  \
+        const auto envValue = EXPR;                                                                                    \
+        if (envValue == WStr("1") || envValue == WStr("true"))                                                         \
+        {                                                                                                              \
+            sValue = 1;                                                                                                \
+        }                                                                                                              \
+        else if (envValue == WStr("0") || envValue == WStr("false"))                                                   \
+        {                                                                                                              \
+            sValue = 0;                                                                                                \
+        }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            sValue = DEFAULT;                                                                                          \
+        }                                                                                                              \
+    }                                                                                                                  \
+    return sValue == 1;
+
+
 #endif // DD_SHARED_MACROS_H_
+
