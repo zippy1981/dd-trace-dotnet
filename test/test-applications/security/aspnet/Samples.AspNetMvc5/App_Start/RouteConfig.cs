b2ab364f4f1b78cdccc0fd41744e52612e4d7894
Robert Pickering <robert@sqreen.io>
2021-07-27 16:19:08 +0200

    Replace our ASP.NET MVC sample with theirs

--- test/test-applications/security/aspnet/Samples.AspNetMvc5/App_Start/RouteConfig.cs
+++ test/test-applications/security/aspnet/Samples.AspNetMvc5/App_Start/RouteConfig.cs
@@ -1,7 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,11 +9,17 @@ namespace Samples.AspNetMvc5
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new
+                          {
+                              controller = "Home",
+                              action = "Index",
+                              id = UrlParameter.Optional
+                          });
         }
     }
 }
