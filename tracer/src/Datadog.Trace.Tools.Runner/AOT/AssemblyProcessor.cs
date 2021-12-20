5f877e651cc41509b73d34afac2c4e84c1243204
Tony Redondo <tony.redondo@datadoghq.com>
2021-12-20 13:36:29 +0100

    changes.

--- tracer/src/Datadog.Trace.Tools.Runner/AOT/AssemblyProcessor.cs
+++ tracer/src/Datadog.Trace.Tools.Runner/AOT/AssemblyProcessor.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using Datadog.Trace.ClrProfiler;
 using Datadog.Trace.Logging;
 using Mono.Cecil;
 
@@ -41,38 +42,131 @@ namespace Datadog.Trace.Tools.Runner.AOT
             using var assemblyDefinition = AssemblyDefinition.ReadAssembly(_assemblyFilePath, new ReaderParameters
             {
                 ReadSymbols = false,
-                ReadWrite = true
+                ReadWrite = true,
             });
 
-            Console.WriteLine(assemblyDefinition.Name.FullName);
+            var currentAssemblyName = assemblyDefinition.Name;
 
             bool isDirty = false;
-
-            var instrumentations = _instrumentation.Where(i =>
+            var version = new IntegrationVersionRange();
+            foreach (var instrumentation in _instrumentation)
             {
-                var asmName = i.AssemblyNames[0];
-                if (asmName != assemblyDefinition.Name.Name)
+                // Check the assembly name
+                var asmName = instrumentation.AssemblyNames[0];
+                if (asmName != currentAssemblyName.Name)
                 {
-                    return false;
+                    continue;
                 }
 
-                return true;
-            }).ToList();
+                version.MinimumVersion = instrumentation.MinimumVersion;
+                version.MaximumVersion = instrumentation.MaximumVersion;
+                var minimumVersion = new Version(version.MinimumMajor, version.MinimumMinor, version.MinimumPatch);
+                var maximumVersion = new Version(version.MaximumMajor, version.MaximumMinor, version.MaximumPatch);
 
-            if (instrumentations.Count == 0)
-            {
-                return;
+                // Check version (minimum version)
+                if (currentAssemblyName.Version < minimumVersion)
+                {
+                    continue;
+                }
+
+                // Check version (maximum version)
+                if (currentAssemblyName.Version > maximumVersion)
+                {
+                    continue;
+                }
+
+                // Process integration
+                isDirty |= SelectAndProcessIntegration(assemblyDefinition, instrumentation);
             }
 
             if (isDirty)
             {
-                assemblyDefinition.Write(new WriterParameters
+                Console.WriteLine($"Done: {filePath}");
+
+                try
                 {
-                    WriteSymbols = true,
-                });
+                    assemblyDefinition.Write(new WriterParameters
+                    {
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(ex.ToString());
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        private bool SelectAndProcessIntegration(AssemblyDefinition assemblyDefinition, InstrumentMethodStruct instrumentation)
+        {
+            var hadInstrumented = false;
+            var mainModule = assemblyDefinition.MainModule;
+            foreach (var moduleType in mainModule.GetTypes())
+            {
+                // Check FullType name
+                if (moduleType.FullName != instrumentation.TypeName)
+                {
+                    continue;
+                }
+
+                foreach (var typeMethod in moduleType.Methods)
+                {
+                    // Check Method name
+                    if (typeMethod.Name != instrumentation.MethodName)
+                    {
+                        continue;
+                    }
+
+                    // Check Return type
+                    if (instrumentation.ReturnTypeName != "_" && typeMethod.ReturnType.FullName != instrumentation.ReturnTypeName)
+                    {
+                        continue;
+                    }
+
+                    // Check methods parameters length
+                    if (typeMethod.Parameters.Count != (instrumentation.ParameterTypeNames?.Length ?? 0))
+                    {
+                        continue;
+                    }
+
+                    // Check methods parameters types
+                    bool isValid = true;
+                    for (int i = 0; i < typeMethod.Parameters.Count; i++)
+                    {
+                        if (instrumentation.ParameterTypeNames[i] == "_")
+                        {
+                            continue;
+                        }
+
+                        if (instrumentation.ParameterTypeNames[i] != typeMethod.Parameters[i].ParameterType.FullName)
+                        {
+                            isValid = false;
+                            break;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        // Process method with integration
+                        hadInstrumented |= ProcessMethodWithIntegration(typeMethod, ref instrumentation);
+                    }
+                }
             }
 
-            Console.WriteLine($"Done: {filePath}");
+            return hadInstrumented;
+        }
+
+        private bool ProcessMethodWithIntegration(MethodDefinition methodDefinition, ref InstrumentMethodStruct instrumentation)
+        {
+            Console.WriteLine("     " + $"{instrumentation.ReturnTypeName} {instrumentation.TypeName}.{instrumentation.MethodName}({string.Join(", ", instrumentation.ParameterTypeNames ?? Array.Empty<string>())})");
+            Console.WriteLine("     " + instrumentation.CallTargetTypeName);
+            Console.WriteLine();
+
+            return true;
         }
     }
 }
